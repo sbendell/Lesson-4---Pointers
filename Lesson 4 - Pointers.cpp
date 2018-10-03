53c40f8ec17d4381dae904dbebd75de2194ee522
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-03 13:28:00 +0100

    Copied Code
    Copied code from tutorials

--- Lesson 4 - Pointers.cpp
+++ Lesson 4 - Pointers.cpp
@@ -2,10 +2,44 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a = 100;
+
+	cout << "Value in memory location a is " << a << "\n";
+
+	cout << "Address of (the reference to) memory location a is " << &a << "\n";
+
+	int *b = &a;
+
+	cout << "Value in memory location pointed to by b is " << *b << "\n";
 
 
-int main()
-{
-    return 0;
+	cout << "Value in memory location b is " << b << "\n";
+	cout << b << " should be the same as the memory location for a: " << &a << "\n";
+	cout << "b is actually at memory location " << &b << endl;
+
+	int** c;
+
+	c = &b;
+	cout << "c is now pointing to what b points to: " << **c << endl;
+
+	cout << "c points to: " << *c << endl;
+
+	cout << "c actually holds the value: " << c << endl;
+	cout << c << " is the same value as b's memory location: " << &b << endl;
+	cout << "c is actually at memory location " << &c << endl;
+
+	cout << "c is eventually retrieving what is in memory location " << **&c << endl;
+	cout << "the value of a is in memory location " << &a << endl;
+
+	int x;
+	cout << "Type in a numebr to finish" << "\n";
+	cin >> x;
+
+	return 0;
 }
-
